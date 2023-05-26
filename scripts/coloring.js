52c257aa48499acf2003164593e357f8ac7f7ba8
Lei Wang <lei4.wang@intel.com>
2023-05-26 05:23:06 -0400

    update

--- scripts/coloring.js
+++ scripts/coloring.js
@@ -24,21 +24,26 @@ const coloring = {
         const preNodes = document.querySelectorAll("body > div > pre");
 
         for (const pre of preNodes) {
+            // change <a></a> back to plain text
+            textContent = "";
             for (let i = 0; i < pre.childNodes.length; i++) {
-                if (pre.childNodes[i].nodeName === "#text") {
-                    var contentList = pre.childNodes[i].textContent.split(/(?=diff --git a)/g);
-                    for (const text of contentList) {
-                        const div = document.createElement("DIV");
-                        div.style.padding = "0";
-                        if (text.startsWith("diff --git a")) {
-                            document.body.appendChild(div);
-                            var diff2htmlUi = new Diff2HtmlUI(div, text, configuration);
-                            diff2htmlUi.draw();
-                        }
-                    }
-                    pre.childNodes[i].textContent = contentList[0];
+                if (pre.childNodes[i].nodeName === "#text" || pre.childNodes[i].nodeName === "A")
+                    textContent += pre.childNodes[i].textContent;
+            }
+
+            // highlight the code
+            var contentList = textContent.split(/(?=diff --git a)/g);
+            for (const text of contentList) {
+                if (text.startsWith("diff --git a")) {
+                    const div = document.createElement("DIV");
+                    div.style.padding = "0";
+                    document.body.appendChild(div);
+                    var diff2htmlUi = new Diff2HtmlUI(div, text, configuration);
+                    diff2htmlUi.draw();
                 }
             }
+            pre.innerHTML = "";
+            pre.appendChild(document.createTextNode(contentList[0]));
         }
     },
 };
